360949336efa87eeb0406990aaace5069ad84aa0
Kiet <bunbothomngon@gmail.com>
2018-10-08 20:27:14 +0300

    slider

--- main.js
+++ main.js
@@ -1,12 +1,56 @@
-console.log(arrayData);
+/*jshint esversion: 6 */
 
 const displayImgs = () => {
-    const imageWrapper = document.querySelector('.imageWrapper');
-    arrayData.forEach( info => {
-        imageWrapper.insertAdjacentHTML("afterbegin",
-        `<img src="images/${info.src}" alt="info.src" width="300px", height="400px">`)
-        console.log(`images/${info.src}`);        
-    })
-}
+  const galleryContainer = document.querySelector('.js-galleryContainer');
 
+  arrayData.forEach(data => {
+    galleryContainer.insertAdjacentHTML(
+      'afterbegin',
+      `
+      <li class="js-photo">
+        <img src="images/${data.src}" class="img-responsive photo">
+      </li>
+      `
+    );
+  });
+};
 displayImgs();
+
+let slideIndex = 1;
+
+// Next/previous controls
+const plusSlides = n => {
+  switchSlides((slideIndex += n));
+};
+
+const switchSlides = n => {
+  const allListItems = document.getElementsByClassName('js-photo');
+  const allImages = document.getElementsByClassName('photo');
+  const list = document.querySelectorAll('.js-photo');
+  // list.classList.add('transform');
+  if (n > allListItems.length) {
+    slideIndex = 1;
+  }
+
+  if (n < 1) {
+    slideIndex = allListItems.length;
+  }
+
+  for (let i = 0; i < allImages.length; i++) {
+    allImages[i].className = allImages[i].className.replace(' active', '');
+  }
+  allImages[slideIndex - 1].className += ' active';
+
+  for (let i = 0; i < allListItems.length; i++) {
+    allListItems[i].className = allListItems[i].className.replace(' transform', '');
+  }
+
+  list.forEach(e => {
+    const style = window.getComputedStyle(e);
+    const matrix = new WebKitCSSMatrix(style.webkitTransform);
+    
+    e.style.transform = `translateX(${matrix.m41 - 250}px)`;
+  });
+};
+
+
